db995c3655686daf393a0bf418bdbaa69512266b
gsingharoy <neogauravsvnit@gmail.com>
2017-08-05 10:29:21 +0200

    Basic implementation of the cache
    Added the basic implementation of the cache.
    It includes two public functions called Set and Find

---
+++ cache.go
@@ -0,0 +1,58 @@
+package onecache
+
+import "time"
+
+type Cache struct {
+	recordMap map[string]record
+	Timestamp time.Time
+}
+
+// Returns a new Cache instance
+func New() *Cache {
+	c := &Cache{
+		recordMap: make(map[string]record),
+		Timestamp: time.Now(),
+	}
+	return c
+}
+
+// Sets an entry to the cached records
+//
+// @params
+// key:         [string] key of the record
+// v:           [interface{}] any record which should be the value of the cache
+// expiresIn:   [int64] time in seconds in which the cached value will be expired
+func (c *Cache) Set(key string, v interface{}, expiresIn int64) {
+	c.recordMap[key] = record{
+		value:     v,
+		timestamp: time.Now(),
+		expiresIn: expiresIn,
+	}
+}
+
+// Finds the record in the cache.
+//
+// @params
+// key: [string] key of the record
+//
+// @return
+// interface{} value of the record
+// bool : indicates if the record has been found or not
+func (c *Cache) Find(key string) (interface{}, bool) {
+	r, ok := c.recordMap[key]
+	if !ok || r.hasExpired() {
+		return nil, false
+	}
+	// To optimize this, this go routine can be a channel which will accept all clean() requests
+	go c.clean()
+	return r.value, true
+}
+
+// deletes expired records
+func (c *Cache) clean() {
+	for k, v := range c.recordMap {
+		if v.hasExpired() {
+			delete(c.recordMap, k)
+		}
+	}
+}
