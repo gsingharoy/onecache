a0730b416dc3b15d5013bfa3382183032e0b73aa
gsingharoy <neogauravsvnit@gmail.com>
2017-08-05 10:40:37 +0200

    Use govendor
    Added vendor dependencies in the /vendor folder using govendor

--- record_test.go
+++ record_test.go
@@ -3,18 +3,16 @@ package onecache
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRecordHasExpired(t *testing.T) {
 	t.Log("When the record has not expired")
 	r1 := &record{expiresIn: 1000, timestamp: time.Now()}
-	if r1.hasExpired() {
-		t.Error("Expected the record to not have expired")
-	}
+	assert.Equal(t, false, r1.hasExpired())
 
 	t.Log("When the record has expired")
 	r2 := &record{expiresIn: 10, timestamp: time.Now().Add(time.Second * time.Duration(11) * -1)}
-	if !r2.hasExpired() {
-		t.Error("Expected the record to have expired")
-	}
+	assert.Equal(t, true, r2.hasExpired())
 }
